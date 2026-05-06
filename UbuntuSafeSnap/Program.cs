72cf08e757146e1919605943f2cdd45665552cd7
Christian Gennari <christiangennari61@gmail.com>
2026-05-06 11:59:59 +0200

    feat: integrate TargetResolverService and add targets.txt starter file
    Wire TargetResolverService into Program entry point, add auto-generating targets.txt with starter content, and add solution file

--- UbuntuSafeSnap/Program.cs
+++ UbuntuSafeSnap/Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using UbuntuSafeSnap;
+using UbuntuSafeSnap.Services;
+
+const string targetsFile = "targets.txt";
+
+if (!File.Exists(targetsFile))
+{
+    File.WriteAllText(targetsFile, "# Add directories here, one per line" + Environment.NewLine + "tests/mock_system" + Environment.NewLine);
+    Console.WriteLine($"[{targetsFile}] did not exist. A starter file has been created.");
+    Console.WriteLine("Please review / edit it, then rerun the program.");
+    Console.Write("Press Enter to exit...");
+    Console.ReadLine();
+    return;
+}
+
+var targetDirectories = TargetResolverService.Resolve(targetsFile).ToArray();
 
 string stagingDirectory = Path.Combine(Directory.GetCurrentDirectory(), "staging");
 
@@ -7,8 +22,6 @@ var packageService = new PackageService();
 await packageService.ExtractPackageListAsync(stagingDirectory);
 
 var configService = new ConfigService();
-await configService.CollectConfigFilesAsync(
-    new[] { Path.Combine(Directory.GetCurrentDirectory(), "tests", "mock_system") },
-    stagingDirectory);
+await configService.CollectConfigFilesAsync(targetDirectories, stagingDirectory);
 
 Console.WriteLine($"Done! Check: {stagingDirectory}");
