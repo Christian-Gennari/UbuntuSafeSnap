379952ed0ef60ee725a1f9c5d986724b9538894b
Christian Gennari <christiangennari61@gmail.com>
2026-05-07 19:18:51 +0200

    fix: make GetDefaultContent static and untrack runtime config files (#28)

--- UbuntuSafeSnap/Interfaces/IExclusionService.cs
+++ UbuntuSafeSnap/Interfaces/IExclusionService.cs
@@ -2,7 +2,6 @@ namespace UbuntuSafeSnap.Interfaces;
 
 public interface IExclusionService
 {
-    string GetDefaultContent();
     void Load(string filePath);
     bool ShouldExclude(string filePath);
 }
