7c2e68cf448af966a2d98274f4095962242dcee8
Christian Gennari <christiangennari61@gmail.com>
2026-05-09 12:50:50 +0200

    refactor: remove --config-path and --non-interactive, add startup validation (#42)
    - Remove --config-path and --non-interactive CLI options from Program.cs
    - Remove TargetResolverService.EnsureExists() and GetDefaultContent()
    - Remove ExclusionService.EnsureExists() and GetDefaultContent()
    - Add startup validation: backup requires targets.txt and exclusions.txt
      in current working directory
    - Error message directs user to run from app home directory
    
    Closes #42

--- UbuntuSafeSnap/Services/TargetResolverService.cs
+++ UbuntuSafeSnap/Services/TargetResolverService.cs
@@ -35,28 +35,4 @@ public class TargetResolverService : ITargetResolverService
             yield return fullPath;
         }
     }
-
-    public static string GetDefaultContent() =>
-        "# Add directories here, one per line" + Environment.NewLine + "tests/mock_system" + Environment.NewLine;
-
-    public static int EnsureExists(string configPath, bool nonInteractive, out string targetsFile)
-    {
-        targetsFile = Path.Combine(configPath, "targets.txt");
-
-        if (File.Exists(targetsFile))
-            return 0;
-
-        if (nonInteractive)
-        {
-            Console.Error.WriteLine($"Required file not found: {targetsFile}");
-            return 1;
-        }
-
-        File.WriteAllText(targetsFile, GetDefaultContent());
-        Console.WriteLine($"[{targetsFile}] did not exist. A starter file has been created.");
-        Console.WriteLine("Please review / edit the created file(s), then rerun the program.");
-        Console.Write("Press Enter to exit...");
-        Console.ReadLine();
-        return 1;
-    }
 }
